cdfdf8155eeaa2a00f302a71a2c86fc1b92061a6
pau.ballber <pau.ballber@est.fib.upc.edu>
2020-05-03 20:52:01 +0200

    Càlcul temps seqüencial, introducció paramètres k i TestPoint mitjançant la línia de comandes

--- knn00.cu
+++ knn00.cu
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <math.h> 
+#include <iostream>     // std::cout
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
+#include <time.h> 
+using namespace std;
+
 #define PINNED 0
 #define THREADS 8
 
@@ -48,10 +55,10 @@ int classifyAPoint(Point arr[], int n, int k, Point p)
     } 
   
     return (freq1 > freq2 ? 0 : 1); 
-} 
+}
 
-int main(int argc, char** argv)
-{
+int classifyAPointCUDA(Point arr[], int n, int k, Point p) 
+{ 
     unsigned int N;
     unsigned int numBytes;
     unsigned int nBlocks, nThreads;
@@ -98,6 +105,135 @@ int main(int argc, char** argv)
     cudaMemcpy(dist_points_dev, dist_points_host,numBytes, cudaMemcpyHostToDevice);
     
     // Ejecutar el kernel 
+    
+}
+
+void InitKDefecte(int *k) {
+    
+    // Parameter to decide group of the testing point 
+    (*k) = 3;
+    
+}
+
+void InitTestPointDefecte(struct Point *p) {
+    
+    //Test Point
+    p->x = 2.5; 
+    p->y = 7; 
+    
+}
+
+void InitDefecte(int *k, struct Point *p) {
+    InitKDefecte(k);
+    InitTestPointDefecte(p);
+}
+
+int main(int argc, char** argv)
+{
+    
+    //Es declaren les variables
+    int n, k; 
+    struct Point p;
+    
+    //S'inicialitza la K, i les coordenades del Testing point
+    if (argc == 1)      { InitDefecte(&k, &p); }
+    else if (argc == 2) { k = atoi(argv[1]); InitTestPointDefecte(&p); }
+    else if (argc == 4) { k = atoi(argv[1]); p.x = atof(argv[2]); p.y = atof(argv[3]);}
+    else { printf("Usage: ./exe k TestPointCoordenadaX TestPointCoordenadaY\n"); exit(0); }
+    
+    //Es crea l'estructura sobre la qual es vol fer la predicció
+    n = 17; // Number of data points 
+    Point arr[n];
+    
+    arr[0].x = 1; 
+    arr[0].y = 12; 
+    arr[0].val = 0; 
+  
+    arr[1].x = 2; 
+    arr[1].y = 5; 
+    arr[1].val = 0; 
+  
+    arr[2].x = 5; 
+    arr[2].y = 3; 
+    arr[2].val = 1; 
+  
+    arr[3].x = 3; 
+    arr[3].y = 2; 
+    arr[3].val = 1; 
+  
+    arr[4].x = 3; 
+    arr[4].y = 6; 
+    arr[4].val = 0; 
+  
+    arr[5].x = 1.5; 
+    arr[5].y = 9; 
+    arr[5].val = 1; 
+  
+    arr[6].x = 7; 
+    arr[6].y = 2; 
+    arr[6].val = 1; 
+  
+    arr[7].x = 6; 
+    arr[7].y = 1; 
+    arr[7].val = 1; 
+  
+    arr[8].x = 3.8; 
+    arr[8].y = 3; 
+    arr[8].val = 1; 
+  
+    arr[9].x = 3; 
+    arr[9].y = 10; 
+    arr[9].val = 0; 
+  
+    arr[10].x = 5.6; 
+    arr[10].y = 4; 
+    arr[10].val = 1; 
+  
+    arr[11].x = 4; 
+    arr[11].y = 2; 
+    arr[11].val = 1; 
+  
+    arr[12].x = 3.5; 
+    arr[12].y = 8; 
+    arr[12].val = 0; 
+  
+    arr[13].x = 2; 
+    arr[13].y = 11; 
+    arr[13].val = 0; 
+  
+    arr[14].x = 2; 
+    arr[14].y = 5; 
+    arr[14].val = 1; 
+  
+    arr[15].x = 2; 
+    arr[15].y = 9; 
+    arr[15].val = 0; 
+  
+    arr[16].x = 1; 
+    arr[16].y = 7; 
+    arr[16].val = 0; 
+    
+    printf("k = %d \n", k);
+    
+    printf("The Testing Point values are:");
+    printf(" x = %lf", p.x);
+    printf(" and");
+    printf(" y = %lf", p.y);
+    printf("\n");
+    
+    // Calculate the time taken by the sequential code: classifyAPoint function 
+    clock_t t; 
+    t = clock(); 
+    int result = classifyAPoint(arr, n, k, p); 
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+    
+    printf ("The value classified to unknown point"
+            " is %d.\n", result);
+            
+    printf ("Temps seqüencial:"
+            " is %lf.\n", time_taken);
+    
 }
 
 
